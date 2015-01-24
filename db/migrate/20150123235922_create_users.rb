7889146f92952581a5665431f0f89a4744b90973
c0d3s <codyknauer@gmail.com>
2015-01-23 18:14:00 -0600

    added user model + migration

--- db/migrate/20150123235922_create_users.rb
+++ db/migrate/20150123235922_create_users.rb
@@ -1,6 +1,9 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
+      t.string  :handle
+      t.string  :password_digest
+      t.integer :karma
 
       t.timestamps
     end
