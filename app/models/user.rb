000345e231fbe4a3761b613eb4377a0fcbd8edcf
c0d3s <codyknauer@gmail.com>
2015-01-23 18:18:46 -0600

    set up associations between messages and users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
 
   attr_accessible :email, :password, :password_confirmation
   validates_presence_of :password, on: :create
+
+  has_many :sent_messages, foreign_key: 'sender_id'
+  has_many :received_messages, foreign_key: 'receiver_id'
 end
