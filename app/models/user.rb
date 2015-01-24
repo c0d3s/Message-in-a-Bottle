6e3e85b23519220e93bb9ea52c007a2e52da3fa5
c0d3s <codyknauer@gmail.com>
2015-01-23 19:59:07 -0600

    created user and message controllers

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ActiveRecord::Base
   has_secure_password
 
-  attr_accessible :email, :password, :password_confirmation
   validates_presence_of :password, on: :create
 
   has_many :sent_messages, foreign_key: 'sender_id'
